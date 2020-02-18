30a4019082147b3fae2e1c59bac7c467fb0037b1
Kevin Tran <ktran050@gmail.com>
2020-02-18 09:53:55 -0800

    cleaned up STORE array structure implemented handleAnswerSubmit implemented handleQuiz implemented handleCounter

--- index.js
+++ index.js
@@ -1,78 +1,144 @@
 const STORE = [
-  {prompt: "Which attraction is not part of Adventureland?",
-  answers: ["Indiana Jones Adventure", "Clayton’s Clay Shoot", "Jungle Cruise", "Tarzan's Adventure", "Enchanted Tiki Room"],
-  imgSrc: "https://www.ocregister.com/wp-content/uploads/2019/05/AdventurelandSign2-1.jpg?w=620",
-  corrAnswer: "yes"},
+  {
+    question: 'Which attraction is not part of Adventureland?',
+    answers: [
+      'Indiana Jones Adventure',
+      "Clayton's Clay Shoot",
+      'Jungle Cruise',
+      "Tarzan's Adventure",
+      'Enchanted Tiki Room'
+    ],
+    img: 'https://www.ocregister.com/wp-content/uploads/2019/05/AdventurelandSign2-1.jpg?w=620',
+    answerIndex: 1
+  },
+  {
+    question: 'When was Disneyland first built?',
+    answers: ['2015', '1920', '1963', '1955', '1986'],
+    img: 'https://user-images.githubusercontent.com/9054819/73940503-7a9b5e00-48e3-11ea-8538-1646ac67976e.jpg',
+    answerIndex: 3
+  }
 ];
 
-let currentQuestion = 0;
-let currentScore = 0; 
+let currentQuestionIndex = 0;
+let currentScore = 0;
 
-function renderForm(){
-  console.log("form rendered");
+function renderForm() {
+  console.log('form rendered');
   renderQuestion();
 }
 
-function prepareAnswers(){
-  let options = "";
-  STORE[currentQuestion].answers.forEach(
-    function (item){
-      options += `
+function prepareAnswers() {
+  let options = '';
+  STORE[currentQuestionIndex].answers.forEach(function(item) {
+    options += `
       <input type="radio"
-      name="q1-ans" id=${item}
-      value=${item}>
+      name="${currentQuestionIndex}-ans" id="${item}"
+      value="${item}">
       <label for="indiana">${item}</label><br />`;
-    }
-  );
+  });
   return options;
 }
 
-function renderQuestion(){
+function renderCounter() {
+  let counterHtml = `score: ${currentScore}`;
+  $('.counter').html(counterHtml);
+  console.log('counter rendered');
+}
+
+function renderQuestion() {
   const answersList = prepareAnswers();
   const questionHTML = `
   <fieldset required>
-    <legend>Question ${currentQuestion + 1} / 5:<br />
-    ${STORE[currentQuestion].prompt} </legend>
+    <legend>Question ${currentQuestionIndex + 1} / 5:<br />
+    ${STORE[currentQuestionIndex].question} </legend>
     <img
-    src="${STORE[currentQuestion].imgSrc}"
+    src="${STORE[currentQuestionIndex].img}"
     class="placeholder"
     alt="placeholder image"
     /><br />
 
     ${answersList}
 
-  </fieldset>`;
+  </fieldset>
+  <button type="submit" class="check-btn">Check Answer</button>
+  `;
   $('.question').html(questionHTML);
-  console.log("question rendered");
+  console.log('question rendered');
 }
 
-function renderResult(){
-  
-  console.log("result rendered");
+function renderResult() {
+  console.log('result rendered');
 }
 
-function renderFinalResult(){
-  console.log("final result rendered");
+function renderFinalResult() {
+  console.log('final result rendered');
 }
 
-function handleAnswerSubmit(){
-  $('#js-quiz-form').submit(
-    function(event){
-      event.preventDefault();
-      console.log("answer submitted");
-      renderResult();
+function handleAnswerSubmit() {
+  $('#js-quiz-form').submit(function(event) {
+    event.preventDefault();
+    let currentQuestion = STORE[currentQuestionIndex];
+    let answerIndex = currentQuestion.answerIndex;
+    let feedback = '';
+    console.log('value: ' + $(`input[name="${currentQuestionIndex}-ans"]:checked`).val());
+    console.log(`answerIndex: ${currentQuestion.answers[answerIndex]}`);
+    if ($(`input[name="${currentQuestionIndex}-ans"]:checked`).val() === currentQuestion.answers[answerIndex]) {
+      //  if form answer !=  answer
+      console.log('right');
+      let buttonText = 'Next';
+      feedback = `
+            <div class="correct feedback">
+              Correct answer!
+            </div>
+          `;
+      $('.sub-btn').html(buttonText);
+      $('.question').append(feedback);
+      ++currentScore;
+      ++currentQuestionIndex;
+      console.log(`currentScore: ${currentScore}`);
+      renderCounter();
+      renderQuestion();
+      // display feedback correct
+    } else {
+      console.log('wrong');
+      // display feedback incorrect
     }
-  );
+  });
 }
 
-function handleQuiz(){
+// function handleAnswerSubmit() {
+//   $('#js-quiz-form').submit(function(event) {
+//     event.preventDefault();
+//     $('button').hasClass('check-btn'){
+//       console.log('check')
+//     }// if questions page
+//       // if true
+//       // else
+//     // if results page
+//   });
+// }
+
+
+function handleQuiz() {
   renderForm();
+  renderCounter();
   handleAnswerSubmit();
 }
 
 handleQuiz();
 
-
 // Wasted time forgetting to link jquery
+// wasted time forgettting the . in '.someClass'
 
-// TODO: value of each item in prepareAnswers is just the first string
+// TODO: value of each item in prepareAnswers is just the first string ~~~~~~~~~~~~~~~~~
+// check submitted answer ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// display feedback based on the answer ~~~~~~~~~~~~~~~~~~~~~~~~
+// increment based on the answer ~~~~~~~~~~~~~~~~~~~~~~
+// lock submits
+// load next question
+// recognize we are finished with the quiz and display results screen
+
+
+// mentor notes:
+// cut down on abbreviations
+// cut redundant data ex. storing the correct answer again in the questions object rather than just the index that the answer is already in 
